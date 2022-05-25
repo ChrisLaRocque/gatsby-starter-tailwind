254bce1f000354fd4995c97296ac275959283dc6
ChrisLaRocque <larocque.christopher@gmail.com>
2022-05-25 11:21:36 -0400

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,139 @@
+import * as React from "react";
+
+// data
+const links = [
+  {
+    text: "Tutorial",
+    url: "https://www.gatsbyjs.com/docs/tutorial/",
+    description:
+      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+    color: "#E95800",
+  },
+  {
+    text: "How to Guides",
+    url: "https://www.gatsbyjs.com/docs/how-to/",
+    description:
+      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+    color: "#1099A8",
+  },
+  {
+    text: "Reference Guides",
+    url: "https://www.gatsbyjs.com/docs/reference/",
+    description:
+      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+    color: "#BC027F",
+  },
+  {
+    text: "Conceptual Guides",
+    url: "https://www.gatsbyjs.com/docs/conceptual/",
+    description:
+      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+    color: "#0D96F2",
+  },
+  {
+    text: "Plugin Library",
+    url: "https://www.gatsbyjs.com/plugins",
+    description:
+      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+    color: "#8EB814",
+  },
+  {
+    text: "Build and Host",
+    url: "https://www.gatsbyjs.com/cloud",
+    badge: true,
+    description:
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+    color: "#663399",
+  },
+];
+
+// markup
+export default function IndexPage() {
+  return (
+    // <main className="container max-w-xl mx-auto py-2">
+    //   <title>Home Page</title>
+    //   <h1 className="text-xl font-extrabold">
+    //     Congratulations
+    //     <br />
+    //     <span>— you just made a Gatsby site! </span>
+    //     <span role="img" aria-label="Party popper emojis">
+    //       🎉🎉🎉
+    //     </span>
+    //   </h1>
+    //   <p>
+    //     Edit <code>src/pages/index.js</code> to see this page update in
+    //     real-time.{" "}
+    //     <span role="img" aria-label="Sunglasses smiley emoji">
+    //       😎
+    //     </span>
+    //   </p>
+    //   <ul>
+    //     {links.map((link) => (
+    //       <li key={link.url} style={{ color: link.color }} className="mb-2">
+    //         <span>
+    //           <a
+    //             href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+    //             className="underline"
+    //           >
+    //             {link.text}
+    //           </a>
+    //           &nbsp;
+    //           <p className="inline">{link.description}</p>
+    //         </span>
+    //       </li>
+    //     ))}
+    //   </ul>
+    //   <img
+    //     alt="Gatsby G Logo"
+    //     src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+    //   />
+    // </main>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold py-16">
+          Welcome to{" "}
+          <a className="text-purple-600" href="https://www.gatsbyjs.com/">
+            Gatsby.js!
+          </a>
+          <span role="img" aria-label="Party popper emojis">
+            🎉🎉🎉
+          </span>
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Get started by editing{" "}
+          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+            pages/index.js
+          </code>
+        </p>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          {links.map((link) => (
+            <a
+              href={link.url}
+              className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-purple-600 focus:text-purple-600"
+            >
+              <h3 className="text-2xl font-bold">{link.text} &rarr;</h3>
+              <p className="mt-4 text-xl">{link.description}</p>
+            </a>
+          ))}
+        </div>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://gatsbyjs.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img
+            alt="Gatsby G Logo"
+            src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+          />
+        </a>
+      </footer>
+    </div>
+  );
+}
